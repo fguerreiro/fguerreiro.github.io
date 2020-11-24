07cb6f2e7c1a8511c2b1dc72260172b53d1ac5fd
Francisco Guerreiro <fguerreiro@coveo.com>
2020-11-24 18:29:47 -0500

    trying python for gc

--- main.go
+++ main.go
@@ -1,10 +1,42 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
-func init() {
-    fs := http.FileServer(http.Dir("web"))
-    http.Handle("/", fs)
+// [START main_func]
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
 }
+
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
+
+// [END indexHandler]
